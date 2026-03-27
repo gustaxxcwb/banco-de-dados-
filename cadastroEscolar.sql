3f6436cc7a43ec49a8a8abd965f3044c170b7ecc
gustaxxcwb <gustavo.schuarca@escola.pr.gov.br>
2026-03-27 07:48:33 -0300

    Create cadastroEscolar.sql

---
+++ cadastroEscolar.sql
@@ -0,0 +1,18 @@
+INSERT INTO Aluno (id_aluno, nome, idade)
+VALUES
+(1, 'Ana Costa', 18),
+(2, 'João Silva', 20),
+(3, 'Maria Oliveira', 19),
+(4, 'Pedro Santos', 21),
+(5, 'Carla Souza', 22);-- Inserindo dados na tabela Professor
+INSERT INTO Professor (id_professor, nome, cpf)
+VALUES 
+(1, 'Ricardo Silva', '12345678901'),
+(2, 'Maria Oliveira', '98765432100'),
+(3, 'Carlos Eduardo', '55544433322');
+-- Inserindo dados na tabela Escola
+INSERT INTO Escola (id_escola, nome_escola, telefone)
+VALUES
+(1, 'Escola Primavera', '11988887777'),
+(2, 'Colégio Horizonte', '11977776666'),
+(3, 'Instituto Saber', '11966665555');
